3cc0c0a9eff796d170c290c848a3f002d948b8f6
Nick Stewart <nick@nickstewart.me>
2015-04-15 11:28:58 -0400

    Changed cache system to store cache in database, added count option, added remove cache option

--- get-tweets-hashtag/get-tweets-hashtag.php
+++ get-tweets-hashtag/get-tweets-hashtag.php
@@ -2,18 +2,52 @@
 /*
 Plugin Name: Get Tweets By Hashtag
 Plugin URI: http://viastudio.com
-Description: Get tweets by hashtag and display them 
-Version: 1
+Description: Get tweets by hashtag and display them
+Version: 1.5
 Author: Nick Stewart
 Author URI: http://www.nickstewart.me
 */
 
-require plugin_dir_path( __FILE__ ) . 'twitteroauth/autoload.php';
+require plugin_dir_path(__FILE__) . 'twitteroauth/autoload.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
+
+global $get_tweets_db_version;
+$get_tweets_db_version = '1.1';
+
+//install tables
+function get_tweets_install($upgrade) {
+    global $wpdb;
+    global $get_tweets_db_version;
+    $table_name = $wpdb->prefix . "get_tweets";
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      hashtag varchar(100) NOT NULL,
+      created varchar(10) NOT NULL,
+      cache TEXT CHARACTER SET ascii NOT NULL,
+      UNIQUE KEY id (id)
+    ) $charset_collate;";
+
+    require_once(ABSPATH . '/wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+
+    if ($upgrade == 'yes') {
+        update_option('get_tweets_db_version', $get_tweets_db_version);
+    } else {
+        add_option('get_tweets_db_version', $get_tweets_db_version);
+    }
+}
+function get_tweets_update() {
+    global $get_tweets_db_version;
+    if (get_site_option('get_tweets_db_version') != $get_tweets_db_version) {
+        get_tweets_install('yes');
+    }
+}
 
 //include css
 function loadcss() {
-    wp_register_style('get-tweets-style', plugins_url('default.css',__FILE__ ));
+    wp_register_style('get-tweets-style', plugins_url('default.css', __FILE__));
     wp_enqueue_style('get-tweets-style');
 }
 loadcss();
@@ -22,29 +56,35 @@ loadcss();
 function get_tweets_createsettings() {
     add_options_page('Get Tweeets by Hashtag Settings', 'Get Tweets by Hashtag', 'manage_options', 'get-tweets', 'get_tweets_settingspage'); //settings menu page
 }
-function get_tweets_registersettings() { // whitelist options
-    register_setting( 'get-tweets-api', 'oauth_access_token' );
-    register_setting( 'get-tweets-api', 'oauth_access_token_secret' );
-    register_setting( 'get-tweets-api', 'consumer_key' );
-    register_setting( 'get-tweets-api', 'consumer_secret' );
+function get_tweets_registersettings() {
+    register_setting('get-tweets-api', 'oauth_access_token');
+    register_setting('get-tweets-api', 'oauth_access_token_secret');
+    register_setting('get-tweets-api', 'consumer_key');
+    register_setting('get-tweets-api', 'consumer_secret');
 }
-if ( is_admin() ) {
+if (is_admin()) {
     add_action('admin_menu', 'get_tweets_createsettings');
-    add_action( 'admin_init', 'get_tweets_registersettings' );
+    add_action('admin_init', 'get_tweets_registersettings');
 }
 
 function get_tweets_settingspage() {
-    echo '<h2>Get Tweets by Hashtag</h2><p>Place shortcode on any page or post for plugin to display. Tweets are cached for 1 hour (in the next update this will be an option)</p>';
-    echo '<b>Shortcode</b><br>[get-tweets hashtag="#yourhashtag"]';
+    if ($_GET['settings-updated'] == 'true') {
+        global $wpdb;
+        $table_name = $wpdb->prefix . "get_tweets";
+        $delete = $wpdb->query("TRUNCATE TABLE $table_name");
+        echo $wpdb->last_error;
+    }
+    echo '<h2>Get Tweets by Hashtag</h2><p>Place shortcode on any page or post for plugin to display. Tweets are cached for 1 hour (to reset cache just click the save changes button)</p>';
+    echo '<b>Shortcodes</b><br><br><code>[get-tweets hashtag="#yourhashtag"]</code><br><br><code>[get-tweets hashtag="#yourhashtag" count="10"]</code> (count is optional, default is set for 15)';
     echo '<style>td{ padding-left:20px; }</style>';
-    echo '<h2>Settings</h2><p>Visit <a href="http://iag.me/socialmedia/how-to-create-a-twitter-app-in-8-easy-steps/" target="_blank">here</a> to learn how to retrieve this information</p>';
+    echo '<h2>Settings</h2><p>Visit <a href="http://iag.me/socialmedia/how-to-create-a-twitter-app-in-8-easy-steps/" target="_blank">here</a> to learn how to retrieve this information for the plugin to work.</p>';
     echo '<form method="post" action="options.php"><table>';
-    settings_fields( 'get-tweets-api' );
-    do_settings_sections( 'get-tweets-api' );
-    echo '<tr><td>Oauth Access Token</td><td><input type="text" name="oauth_access_token" value="' . esc_attr( get_option('oauth_access_token') ) . '" />';
-    echo '<tr><td>Oauth Access Secret</td><td><input type="text" name="oauth_access_token_secret" value="' . esc_attr( get_option('oauth_access_token_secret') ) . '" />';
-    echo '<tr><td>Consumer Key</td><td><input type="text" name="consumer_key" value="' . esc_attr( get_option('consumer_key') ) . '" />';
-    echo '<tr><td>Consimer Secret</td><td><input type="text" name="consumer_secret" value="' . esc_attr( get_option('consumer_secret') ) . '" />';
+    settings_fields('get-tweets-api');
+    do_settings_sections('get-tweets-api');
+    echo '<tr><td>Oauth Access Token</td><td><input type="text" name="oauth_access_token" value="' . esc_attr(get_option('oauth_access_token')) . '" />';
+    echo '<tr><td>Oauth Access Secret</td><td><input type="text" name="oauth_access_token_secret" value="' . esc_attr(get_option('oauth_access_token_secret')) . '" />';
+    echo '<tr><td>Consumer Key</td><td><input type="text" name="consumer_key" value="' . esc_attr(get_option('consumer_key')) . '" />';
+    echo '<tr><td>Consimer Secret</td><td><input type="text" name="consumer_secret" value="' . esc_attr(get_option('consumer_secret')) . '" />';
     echo '</table>';
     submit_button();
     echo '</form>';
@@ -53,45 +93,57 @@ function get_tweets_settingspage() {
 }
 //cleans up hashtag
 function clean($string) {
-   $string = str_replace(' ', '-', $string); 
-   return preg_replace('/[^A-Za-z0-9\-]/', '', $string);
+    $string = str_replace(' ', '-', $string);
+    return preg_replace('/[^A-Za-z0-9\-]/', '', $string);
 }
 //creates the cache file and checks to see if it needs to be recreated
-function get_tweets_cache($hashtag) {
-    $cachefile = plugin_dir_path( __FILE__ ) . 'twitterhashtag_' . clean($hashtag) . '.txt'; //stores cache as a text file filled with json
-    if (file_exists($cachefile)) {
-        $cachedate = date("H", filemtime($cachefile));
-        $cacheoverwrite = date("H", strtotime('+1 hours'));
-        //compare cache creation/edit date with 1 hour from it, if it is that hour it creates a new cache
-        if ($cachedate >= $cacheoverwrite) { 
-            $current = file_get_contents($cachefile);
-            file_put_contents($cachefile, json_encode(get_tweets_find($hashtag)));
-            fclose($cachefile);
+function get_tweets_cache($hashtag, $count) {
+    global $wpdb;
+    $table_name = $wpdb->prefix . "get_tweets";
+    $results = $wpdb->get_results("select * from $table_name where hashtag = '" . $hashtag . "' LIMIT 1");
+    if (count($results) > 0) {
+        if (($results['0']->created) < date('H')) {
+            $tweets = base64_encode(gzcompress(serialize(get_tweets_find($hashtag, $count))));
+            $wpdb->insert(
+                $table_name,
+                array(
+                    'hashtag' => $hashtag,
+                    'created' => date('H'),
+                    'cache' => $tweets
+                )
+            );
         }
     } else {
-        $cachefile = fopen($cachefile, "w");
-        fwrite($cachefile, json_encode(get_tweets_find($hashtag)));
-        fclose($cachefile);
+        $tweets = base64_encode(gzcompress(serialize(get_tweets_find($hashtag, $count))));
+        $wpdb->insert(
+            $table_name,
+            array(
+                'hashtag' => $hashtag,
+                'created' => date('H'),
+                'cache' => $tweets
+            )
+        );
     }
 }
 //makes call to Twitter and retrives the response
-function get_tweets_find($hashtag) {
-    if (file_exists(plugin_dir_path( __FILE__ ) . 'twitteroauth/autoload.php')) {
+function get_tweets_find($hashtag, $count) {
+    if (file_exists(plugin_dir_path(__FILE__) . 'twitteroauth/autoload.php')) {
         $settings = array(
             'oauth_access_token' => get_option('oauth_access_token'),
             'oauth_access_token_secret' => get_option('oauth_access_token_secret'),
             'consumer_key' => get_option('consumer_key'),
-            'consumer_secret' => get_option('consumer_secret'),
+            'consumer_secret' => get_option('consumer_secret')
         );
         $connection = new TwitterOAuth($settings['consumer_key'], $settings['consumer_secret'], $settings['oauth_access_token'], $settings['oauth_access_token_secret']);
         $content = $connection->get("account/verify_credentials");
-        $twitterdata = $connection->get('search/tweets', array('q' => $hashtag));        
+        $twitterdata = $connection->get('search/tweets', array('q' => $hashtag, 'count' => $count));
 
         $tweets = array();
         $i = 0;
         //only taking what we need
         foreach ($twitterdata->statuses as $tweet) {
-            $tweets[$i]['creation'] = $tweet->created_at;
+            $tweets[$i]['tweet_creation'] = $tweet->created_at;
+            $tweets[$i]['tweet_id'] = $tweet->id_str;
             $tweets[$i]['text'] = $tweet->text;
             $tweets[$i]['user_id'] = $tweet->user->screen_name;
             $tweets[$i]['user_name'] = $tweet->user->name;
@@ -99,35 +151,39 @@ function get_tweets_find($hashtag) {
             $i++;
         }
         return $tweets;
-    } else{
+    } else {
         echo 'Please reinstall plugin... missing files';
     }
 }
 //reads from the cache
-function get_tweets_get($hashtag){
-    get_tweets_cache($hashtag);
-    $filename = plugin_dir_path( __FILE__ ) . 'twitterhashtag_' . clean($hashtag) . '.txt';
-    $handle = fopen($filename, 'r');
-    $tweets = json_decode(fread($handle, filesize($filename)));
-    //fclose($filename);
-    //displays tweet
+function get_tweets_get($hashtag, $count) {
+    get_tweets_cache($hashtag, $count);
+    global $wpdb;
+    $table_name = $wpdb->prefix . "get_tweets";
+    $results = $wpdb->get_results("select * from $table_name where hashtag = '" . $hashtag . "' LIMIT 1");
+    $tweets = unserialize(gzuncompress(base64_decode($results['0']->cache)));
     foreach ($tweets as $tweet => $key) {
-        echo '<div class="quick-tweet"><div class="quick-tweet-avatar"><img src="' . $tweets[$tweet]->user_pic . '"></div>';
-        echo '<div class="quick-tweet-name"><b>' . $tweets[$tweet]->user_name . '</b></div>';
-        echo '<div class="quick-tweet-text">' . $tweets[$tweet]->text . '</div>';
+        echo '<div class="quick-tweet"><div class="quick-tweet-avatar"><img src="' . $tweets[$tweet]['user_pic'] . '"></div>';
+        echo '<div class="quick-tweet-name"><a href="https://twitter.com/' . $tweets[$tweet]['user_id'] . '/status/' . $tweets[$tweet]['tweet_id'] . '">';
+        echo '<b>' . $tweets[$tweet]['user_name'] . '</b></a></div>';
+        echo '<div class="quick-tweet-text">' . $tweets[$tweet]['text'] . '</div>';
         echo '</div>';
     }
 }
 //shortcode function
-function get_tweets_shortcode( $atts, $content = null)  {
-    extract( shortcode_atts( array( 'hashtag' => ''), $atts ) );
+function get_tweets_shortcode($atts, $content = null) {
+    extract(shortcode_atts(array( 'hashtag' => '', 'count' => ''), $atts));
     //check to make sure all thte settings are filled
-    if(strlen(get_option('oauth_access_token')) > 10) {
-        if(strlen(get_option('oauth_access_token_secret')) > 10) {
-            if(strlen(get_option('consumer_key')) > 10) {
-                if(strlen(get_option('consumer_secret')) > 10) {
-                    if($hashtag) {
-                        get_tweets_get($hashtag);
+    if (strlen(get_option('oauth_access_token')) > 10) {
+        if (strlen(get_option('oauth_access_token_secret')) > 10) {
+            if (strlen(get_option('consumer_key')) > 10) {
+                if (strlen(get_option('consumer_secret')) > 10) {
+                    if ($hashtag) {
+                        if ($count > 0) {
+                            get_tweets_get($hashtag, $count);
+                        } else {
+                             get_tweets_get($hashtag, '15');
+                        }
                     } else {
                         echo 'Please use add a hashtag to search.. [get-tweets hashtag="#nfl"] .. for example';
                     }
@@ -144,4 +200,9 @@ function get_tweets_shortcode( $atts, $content = null)  {
         echo 'Please fill out the oauth_access_token setting';
     }
 }
+
 add_shortcode('get-tweets', 'get_tweets_shortcode');
+
+register_activation_hook(__FILE__, 'get_tweets_install');
+//add_action( 'plugins_loaded', 'get_tweets_update' );
+
